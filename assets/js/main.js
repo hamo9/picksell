b90dd1f810aa59603e9a7b8b71230a6d57621d14
hamo9 <hamadashahatali100@gmail.com>
2022-06-27 02:21:01 +0200

    orders page => [ ar , en ]

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// for navbar 
+function toggleDiv(){ 
+    let notification = document.getElementById("notifications");
+    if(notification.style.display === "none"){
+        notification.style.display = "block"
+    }else{
+        notification.style.display = "none"
+    }
+}
+// end nav bar
