e23b6bf6ff2618e98a2d31219a8e4bc0a1c5a6bf
hamo9 <hamadashahatali100@gmail.com>
2022-07-03 23:39:25 +0200

    end navbar compaints page and liks page

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,24 +9,26 @@ function toggleDiv(){
 }
 // end nav bar
 
-
 $(document).ready(function(){
-    $('.nav_box_notification a').click(function(){
+    $('.nav_box_notification i').click(function(){
         $('.container_drop_profile').addClass('d-none');
         $('.container_drop_noti').toggleClass('d-none');
         $('.nav_box_notification a i').toggleClass('active');
-        
     });
 
     $('.navbar_drop_profile a').click(function(){
         $('.container_drop_noti').addClass('d-none');
         $('.container_drop_profile').toggleClass('d-none');
         $('.navbar_drop_profile a i').toggleClass('active');
+        $('.nav_box_notification a i').removeClass('active');
+        
+    });
+
+    $('.under_nav_disktop .item_one').click(function(){
+        $('.box_drob_doun_under').toggleClass('d-none');
         
     });
 
 
 
-    
 });
-
